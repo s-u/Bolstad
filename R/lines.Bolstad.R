73d9f62dcdfd5f759ee7d52b404211ec6ac1342b
dbar344 <dbar344@aucklanduni.ac.nz>
2016-01-18 21:52:53 +1300

    Fixed method line of documentation

--- R/lines.Bolstad.R
+++ R/lines.Bolstad.R
@@ -2,12 +2,13 @@
 #' 
 #' Allows simple addition of posterior distributions from other results to an
 #' existing plot
+#' 
 #' @param x an object of class \code{Bolstad}.
 #' @param \dots any additional parameters to be passed to \code{graphics::lines}.
-#' @method lines.Bolstad
 #' 
-#' @describeIn lines Lines method for Bolstad objects
-#' @export lines.Bolstad
+#' @method lines Bolstad
+#' 
+#' @export
 
 lines.Bolstad = function(x, ...)
   lines(x$param.x, x$posterior, ...)
