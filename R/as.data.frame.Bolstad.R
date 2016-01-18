d1b5245b9085e27a01ea9ee72f5b027f149dea48
dbar344 <dbar344@aucklanduni.ac.nz>
2016-01-18 22:05:10 +1300

    Removed probs from parameters

--- R/as.data.frame.Bolstad.R
+++ R/as.data.frame.Bolstad.R
@@ -1,6 +1,5 @@
 #' as.data.frame.Bolstad
 #' @param x an object of class \code{Bolstad}
-#' @param probs numeric vector of probabilities with values in \eqn{[0,1]}.
 #' @param \dots, any extra arguments needed.
 #' @export as.data.frame.Bolstad
 as.data.frame.Bolstad = function(x, ...){
