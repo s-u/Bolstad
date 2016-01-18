aa92b9c8c12524dfcdd9d1555076fa738bfe9c84
dbar344 <dbar344@aucklanduni.ac.nz>
2016-01-18 21:28:01 +1300

    Added code to check if prior is given or not

--- R/print.summary.Bolstad.R
+++ R/print.summary.Bolstad.R
@@ -19,21 +19,31 @@ print.summary.Bolstad = function(x, digits = max(3L, getOption("digits") - 3L), 
   print(rq, digits = digits, ...)
   
   cat("\nCoefficients:\n")
-  coef.mat = coef(x)
+  coef.mat = matrix(nrow = x$rank, ncol = 3)
+  
+  colnames(coef.mat) = c("Posterior Mean", "Std. Error", "t value")
+  rownames(coef.mat) = c(x$terms)
+  
+  coef.mat[, "Posterior Mean"] = x$coef
+  coef.mat[, "Std. Error"] = x$std.err
+  coef.mat[, "t value"] = x$coef / x$std.err
+  
   print(format(coef.mat, digits = digits), print.gap = 2L, quote = FALSE, ...)
   
   cat("---\n")
   
-  if (x$prior) {
+  if (!is.null(x$prior)) {
+    prior.coef = x$prior$b0
     cat("Prior Coefficients:\n")
-    names(x$prior.coef) = x$terms
-    print(format(x$prior.coef, digits = digits), print.gap = 2L, quote = FALSE, ...)
-    
+    names(prior.coef) = x$terms
+    print(format(prior.coef, digits = digits), print.gap = 2L, quote = FALSE, ...)
+
+    prior.cov = x$prior$V0
     cat("\nPrior Covariance Matrix:\n")
-    dimnames(x$prior.cov) = list(x$terms, x$terms)
-    print(format(x$prior.cov, digits = digits), print.gap = 2L, quote = FALSE, ...)
-    
-  } else {
+    dimnames(prior.cov) = list(x$terms, x$terms)
+    print(format(prior.cov, digits = digits), print.gap = 2L, quote = FALSE, ...)
+  } 
+  else {
     cat("\nNote: No prior given (Using flat prior).\n")
   }
 }
